3efe1d4d18cba93369bc38cc670780cc4192f1a6
TeamT-02 <xumoyuntuxtayev29@gmail.com>
2024-02-01 12:27:12 +0500

    80%

---
+++ main/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Home)
+admin.site.register(Statik)
+admin.site.register(Social_Networks)
+admin.site.register(Social_Networks_link)
+admin.site.register(Problems_left)
+admin.site.register(Problems_right)
+admin.site.register(Website_update)
